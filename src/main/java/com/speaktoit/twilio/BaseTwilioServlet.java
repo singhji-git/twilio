c60a6276e2d53481e7c583d6fd37d3955ea715ea
Artem Goncharuk <artemgoncharuk@speaktoit.com>
2015-05-19 12:33:05 -0400

    Update BaseTwilioServlet.java

--- src/main/java/com/speaktoit/twilio/BaseTwilioServlet.java
+++ src/main/java/com/speaktoit/twilio/BaseTwilioServlet.java
@@ -1,4 +1,25 @@
-package com.speaktoit.twilio;
+package api.ai.twilio;
+
+/***********************************************************************************************************************
+ *
+ * API.AI Twilio Helper Libs
+ * =================================================
+ *
+ * Copyright (C) 2015 by Speaktoit, Inc / Api.ai (https://speaktoit.com)
+ * https://api.ai
+ *
+ ***********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ ***********************************************************************************************************************/
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -62,6 +83,7 @@ public class BaseTwilioServlet extends HttpServlet {
 
 			response = POOLING_HTTP_CLIENT.execute(RequestBuilder.post().setUri(TwilioProperties.INSTANCE.getApiUrl())
 					.addHeader("Authorization", "Bearer " + TwilioProperties.INSTANCE.getApiAccessToken())
+					.addHeader("ocp-apim-subscription-key",  TwilioProperties.INSTANCE.getSubscriptionKey())
 					.addHeader("Accept", ContentType.APPLICATION_JSON.getMimeType()).setEntity(input).build());
 
 			if (response.getStatusLine().getStatusCode() / 100 != 2) {
