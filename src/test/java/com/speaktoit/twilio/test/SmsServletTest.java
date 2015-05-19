b3eddd09dc1a912519274b1ae4fce351cbee18e1
Artem Goncharuk <artemgoncharuk@speaktoit.com>
2015-05-19 12:36:54 -0400

    Update SmsServletTest.java

--- src/test/java/com/speaktoit/twilio/test/SmsServletTest.java
+++ src/test/java/com/speaktoit/twilio/test/SmsServletTest.java
@@ -1,4 +1,25 @@
-package com.speaktoit.twilio.test;
+package ai.api.twilio.test;
+
+/***********************************************************************************************************************
+ *
+ * API.AI Twilio Helper Libs
+ * =================================================
+ *
+ * Copyright (C) 2015 by Speaktoit, Inc / Api.ai (https://speaktoit.com)
+ * https://api.ai
+ *
+ ***********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ ***********************************************************************************************************************/
 
 import static org.junit.Assert.assertEquals;
 
@@ -12,7 +33,7 @@ import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
-import com.speaktoit.twilio.TwilioSmsServlet;
+import ai.api.twilio.TwilioSmsServlet;
 
 public class SmsServletTest {
 	private TwilioSmsServlet servlet;
